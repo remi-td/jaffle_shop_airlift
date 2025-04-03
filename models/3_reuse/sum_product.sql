686be2e7ec31c66eea7348d3e1b039e4f2a3c61e
Rémi Turpaud <remi.turpaud@teradata.com>
2025-04-03 16:23:40 +0000

    fixed history models

--- models/3_reuse/sum_product.sql
+++ models/3_reuse/sum_product.sql
@@ -17,8 +17,9 @@ the valid time period calculation is implemented by the materialisation strategy
 #}
 
 select
-    p.product_cd
-    ,p.product_name
+    --the history logic expects LATIN ... this will need to be fixed
+    translate(cast(p.product_cd as VARCHAR(10) NOT CASESPECIFIC) USING UNICODE_TO_LATIN) product_cd
+    ,translate(cast(p.product_name as VARCHAR(200) NOT CASESPECIFIC) USING UNICODE_TO_LATIN) product_name
     -- The mesure is valid as of the latest order time and until changed
     ,period(max(order_dttm), ('9999-12-31 23:59:59.999999' (timestamp))) valid_period
     ,sum(o.quantity) product_order_cnt
