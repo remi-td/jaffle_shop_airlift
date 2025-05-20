122fb27d8f1e8541bf73cf31b783f81862e8d041
agustin garcia <Agustin.Garcia@Teradata.com>
2025-05-20 17:55:32 +0000

    customer email

--- models/4_consumption/cons_payment_control.sql
+++ models/4_consumption/cons_payment_control.sql
@@ -1,20 +1,25 @@
-{{ config(
-    materialized='otf_materialize',
-    datalake=var('otf_datalake'),
-    datalake_database=var('otf_database'),
-    incremental_strategy='delete+insert',
-    unique_key='source_system_payment_id'
-) 
+{{
+    config(
+        materialized="otf_materialize",
+        datalake=var("otf_datalake"),
+        datalake_database=var("otf_database"),
+        incremental_strategy="delete+insert",
+        unique_key="source_system_payment_id",
+    )
 }}
 
-select 
-p.payment_tstmp payment_dttm
-,o.order_dttm
-,p.payment_amount
-,o.checkout_sum order_checkout_sum
-,o.order_status
-,p.id source_system_payment_id
---add the masked email from the customer reference entity here
-from {{ ref('raw_payments') }} p
-left join {{ var('otf_datalake') }}.{{ ref('disc_order') }} o
-    on o.order_key = p.order_id
+select
+    p.payment_tstmp payment_dttm,
+    o.order_dttm,
+    p.payment_amount,
+    o.checkout_sum order_checkout_sum,
+    o.order_status,
+    p.id source_system_payment_id,
+    c.email
+-- add the masked email from the customer reference entity here
+from {{ ref("raw_payments") }} p
+left join
+    {{ var("otf_datalake") }}.{{ ref("disc_order") }} o on o.order_key = p.order_id
+inner join    {{ref("ref_customer")}} c
+on o.customer_key=c.customer_key
+
