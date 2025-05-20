83b932621f7881162e3f67e808e6f3731dbb104b
Yuta Fukuda <Yuta.Fukuda@Teradata.com>
2025-05-20 07:37:47 +0000

    error fix

--- models/4_consumption/cons_payment_control.sql
+++ models/4_consumption/cons_payment_control.sql
@@ -19,5 +19,5 @@ p.payment_tstmp payment_dttm
 from {{ ref('raw_payments') }} p
 left join {{ var('otf_datalake') }}.{{ ref('disc_order') }} o
     on o.order_key = p.order_id
-left outer join {{ var('otf_datalake') }}.{{ ref('ref_customer')}} c
+left outer join {{ ref('ref_customer')}} c
     on o.customer_key = c.customer_key
