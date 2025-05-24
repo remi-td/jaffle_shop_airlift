bb463955d9d64d1ee7e8fa37ed88d10abfeef2ac
Mohamed Saied <Mohamed.Said@Teradata.com>
2025-05-24 19:04:29 +0000

    change_cons_customer_profile

--- models/4_consumption/cons_customer_profile.sql
+++ models/4_consumption/cons_customer_profile.sql
@@ -10,11 +10,11 @@ with customer_favourite_products as
         sel customer_key
         ,product_cd
         ,rank() over(partition by customer_key order by customer_order_cnt) prod_rank
-        from {{ref('sum_customer_order')}} 
+        from al210ghxMjTZ.sum_customer_order
         where valid_period contains current_timestamp
         qualify prod_rank <=3
     ) o
-    join {{ref('sum_product')}} p
+    join al210ghxMjTZ.sum_product p
         on o.product_cd=p.product_cd
         and p.valid_period contains current_timestamp
     group by 1
@@ -35,12 +35,12 @@ with customer_favourite_products as
         ,product_order_cnt
         ,rank() over(partition by customer_key order by product_order_cnt) prod_rank
         --all product x customer combinations
-        from {{ref('sum_product')}} p
-        cross join {{ref('ref_customer')}} c
+        from al210ghxMjTZ.sum_product p
+        cross join "al210ghxMjTZ"."disc_customers" c
         where p.valid_period contains current_timestamp
         --...that have not yet been seen in orders
         and not exists
-        (sel 1 from {{ref('sum_customer_order')}} o where o.product_cd=p.product_cd and  c.customer_key=o.customer_key)
+        (sel 1 from al210ghxMjTZ.sum_customer_order o where o.product_cd=p.product_cd and  c.customer_key=o.customer_key)
         qualify prod_rank <=3
     ) p
     group by 1    
@@ -58,7 +58,7 @@ with customer_favourite_products as
         customer_key
         ,max(begin(valid_period)) last_order
         ,min(begin(valid_period)) first_order
-        from {{ref('sum_customer_order')}} 
+        from al210ghxMjTZ.sum_customer_order 
         group by 1
     ) a
 )
@@ -69,7 +69,7 @@ with customer_favourite_products as
     customer_key
     ,sum(customer_order_amt) customer_value_amt
     ,percent_rank() over(order by customer_value_amt) value_rank
-    from {{ref('sum_customer_order')}} 
+    from al210ghxMjTZ.sum_customer_order
     group by 1
     where valid_period contains current_timestamp
 )
@@ -77,14 +77,14 @@ with customer_favourite_products as
 ,final as (
 
     select
-        customer.customer_key,
+        customer.customer_key as customer_key,
         'Last ordered '||trim(customer_timeline.last_order_days_ago (int))||' days ago' last_order,
         'Has been customer for '||trim(customer_timeline.customer_for_month  (int))||' months' customer_since,
         'Customer value is '||trim(round(customer_value.value_rank*10,1) (decimal(3,1)))|| '/ 10' as customer_value,
         'Customer favourite products '||customer_favourite_products.top_ordered_products customer_favourite_products,
         'Customer should try '||everyonelse_favourite_products.top_ordered_products everyonelse_favourite_products
 
-    from {{ref('ref_customer')}} customer
+    from "al210ghxMjTZ"."disc_customers" customer
     left join  customer_timeline on customer_timeline.customer_key=customer.customer_key
     left join  customer_favourite_products on customer_favourite_products.customer_key=customer.customer_key
     left join  everyonelse_favourite_products on everyonelse_favourite_products.customer_key=customer.customer_key
