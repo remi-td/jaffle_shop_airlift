47dbff01e5cacadfe43778cca44201769c8de2fc
Mohamed Saied <Mohamed.Said@Teradata.com>
2025-05-24 18:36:14 +0000

    change_ref_customer

--- models/3_reuse/ref_customer.sql
+++ models/3_reuse/ref_customer.sql
@@ -8,6 +8,6 @@ and this makes the model on this layer coherent for its users.
 locking row for access
 select
     customer_key
-    ,REGEXP_REPLACE(customer_nk, '^(.{3})[^@]*(@.*)$', '\1***\2', 1, 1, 'i') AS email --partially mask the email
-from {{ ref('key_customer') }}
-where domain_cd = 'retail'
+    ,REGEXP_REPLACE(email, '^(.{3})[^@]*(@.*)$', '\1***\2', 1, 1, 'i') AS email --partially mask the email
+from al210ghxMjTZ.disc_customers 
+--where domain_cd = 'retail'
