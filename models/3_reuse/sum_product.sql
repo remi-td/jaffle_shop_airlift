f28fcdd8cbf61eb4c6102251c51c52ba8f59f35a
Mohamed Saied <Mohamed.Said@Teradata.com>
2025-05-24 18:48:58 +0000

    change_sum_product

--- models/3_reuse/sum_product.sql
+++ models/3_reuse/sum_product.sql
@@ -18,13 +18,13 @@ the valid time period calculation is implemented by the materialisation strategy
 
 select
     --the history logic expects LATIN ... this will need to be fixed
-    translate(cast(p.product_cd as VARCHAR(10) NOT CASESPECIFIC) USING UNICODE_TO_LATIN) product_cd
+    translate(cast(p.product_code as VARCHAR(10) NOT CASESPECIFIC) USING UNICODE_TO_LATIN) product_cd
     ,translate(cast(p.product_name as VARCHAR(200) NOT CASESPECIFIC) USING UNICODE_TO_LATIN) product_name
     -- The mesure is valid as of the latest order time and until changed
-    ,period(max(order_dttm), ('9999-12-31 23:59:59.999999' (timestamp))) valid_period
+    ,period(max(order_tstmp), ('9999-12-31 23:59:59.999999' (timestamp))) valid_period
     ,sum(o.quantity) product_order_cnt
     ,sum(o.checkout_sum) product_order_amt
-from {{ var('otf_datalake') }}.{{ ref('disc_product_catalog') }} p
-left join {{ var('otf_datalake') }}.{{ ref('disc_order') }} o
-    on o.product_cd = p.product_cd
+from "al210ghxMjTZ"."disc_product_catalog" p
+left join "al210ghxMjTZ"."disc_orders" o
+    on o.product_code = p.product_code
 group by 1, 2
