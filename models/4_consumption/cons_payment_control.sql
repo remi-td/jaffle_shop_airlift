b64283b7b2835ad41b08bf63956d147a8fef7307
Mohamed Saied <Mohamed.Said@Teradata.com>
2025-05-24 19:00:07 +0000

    change_cons_payment_control

--- models/4_consumption/cons_payment_control.sql
+++ models/4_consumption/cons_payment_control.sql
@@ -9,12 +9,14 @@
 
 select 
 p.payment_tstmp payment_dttm
-,o.order_dttm
+,o.order_tstmp order_dttm
 ,p.payment_amount
 ,o.checkout_sum order_checkout_sum
 ,o.order_status
 ,p.id source_system_payment_id
---add the masked email from the customer reference entity here
-from {{ ref('raw_payments') }} p
-left join {{ var('otf_datalake') }}.{{ ref('disc_order') }} o
-    on o.order_key = p.order_id
+,c.email
+from al210ghxMjTZ.disc_payments p
+left join al210ghxMjTZ.disc_orders o
+    on o.id = p.order_id
+left join al210ghxMjTZ.disc_customers c
+    on o.customer_id = c.customer_key
